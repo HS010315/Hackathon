aa62671e65ee3cbf1a1a4c804b148dd86e7119c3
pswxxk <sangwook1205@g.shingu.ac.kr>
2024-07-31 16:06:16 +0900

    Feat: 스마트폰 기초세팅

--- Hackathon_VES/Assets/02_Scripts/UI/SmartPhone.cs
+++ Hackathon_VES/Assets/02_Scripts/UI/SmartPhone.cs
@@ -5,37 +5,35 @@ using UnityEngine.UI;
 
 public class SmartPhone : MonoBehaviour
 {
-    public GameObject smartphoneCanvas; 
     public List<Button> mainButtons;                //스마트폰 메인 창에 표시 될 모든 버튼
     public List<GameObject> panels;                   //스마트폰에 표시될 모든 캔버스
     public Slider battery;                          //스마트폰 배터리 
     private bool isStop = false;
 
-    private void Start()
-    {
-        foreach(var panel in panels)
-        {
-            panel.SetActive(false);
-        }
-    }
     void Update()
     {
-        if(!isStop && Input.GetKeyDown(KeyCode.Escape))
+        if (!isStop && Input.GetKeyDown(KeyCode.Escape))
         {
             Time.timeScale = 0f;
-            smartphoneCanvas.SetActive(true);
+            foreach (var panel in panels)
+            {
+                panel.SetActive(true);
+            }
             isStop = true;
         }
-        else if(isStop && Input.GetKeyDown(KeyCode.Escape))
+        else if (isStop && Input.GetKeyDown(KeyCode.Escape))
         {
             Time.timeScale = 1f;
-            smartphoneCanvas.SetActive(false);
+            foreach (var panel in panels)
+            {
+                panel.SetActive(false);
+            }
             isStop = false;
         }
     }
     void OnButtonClick()
     {
-        switch(mainButtons.Count)
+        switch (mainButtons.Count)
         {
             case 0:
                 //문자 창 전환
